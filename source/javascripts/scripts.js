8ba46ef75b5809d7f0e301afd51e3751b4ae757c
alauderdale <alauderdale@mac.com>
2013-10-22 20:40:14 -0600

    first

---
+++ source/javascripts/scripts.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+
+$( "#menu-button" ).click(function() {
+  $( ".menu-drop" ).toggleClass( "menu-show" );
+  $( "body" ).toggleClass( "nav-open" );
+  $( "#menu-button i" ).toggleClass( "icon-reorder icon-remove" );
+
+});
+
+});
